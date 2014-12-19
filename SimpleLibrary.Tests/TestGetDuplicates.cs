3d2eadcb556be6715e6ed44130b7d0933eaf6d7b
jx <jose.diaz@cdicorp.com>
2014-12-19 16:50:32 -0700

    Implement GetDuplicates() with passing tests

--- SimpleLibrary.Tests/TestGetDuplicates.cs
+++ SimpleLibrary.Tests/TestGetDuplicates.cs
@@ -8,17 +8,56 @@ namespace SimpleLibrary.Tests
     [TestClass]
     public class TestGetDuplicates
     {
+        StringUtilities _stringUtils;
+
+        [TestInitialize]
+        public void Setup ()
+        {
+            _stringUtils = new StringUtilities();
+        }
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException), "Input string is missing")]
         public void Test_GetDuplicates_Shows_Error_When_String_Is_Missing()
         {
-            Assert.Inconclusive("test pending");
+            try
+            {
+                _stringUtils.GetDuplicates(null);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "Input string is empty")]
+        public void Test_GetDuplicates_Shows_Error_When_String_Is_Empty()
+        {
+            try
+            {
+                _stringUtils.GetDuplicates(string.Empty);
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
         }
 
         [TestMethod]
         public void Test_GetDuplicates_Returns_All_Duplicates()
         {
-            Assert.Inconclusive("test pending");
+            string input = "asdfasdf";
+            try
+            {
+                Assert.AreEqual("asdf", _stringUtils.GetDuplicates(input));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
--- StringDuplicates/StringUtilities.cs
+++ StringDuplicates/StringUtilities.cs
@@ -10,7 +10,19 @@ namespace SimpleLibrary
     {
         public string GetDuplicates(string s)
         {
-            return string.Empty;
+            if (String.IsNullOrEmpty(s)) throw new ArgumentNullException("s");
+
+            List<char> processed = new List<char>();
+            StringBuilder dups = new StringBuilder();
+
+            foreach (var chr in s.ToCharArray())
+            {
+                if (processed.Contains(chr))
+                    dups.Append(chr.ToString());
+
+                processed.Add(chr);
+            }
+            return dups.ToString();
         }
     }
 }
