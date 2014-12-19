e988c90435c9efc231030e4ae8eb54c75f9ad21e
jx <jose.diaz@cdicorp.com>
2014-12-19 16:07:42 -0700

    initial project structure

---
+++ SimpleLibrary.Tests/TestGetDuplicates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SimpleLibrary;
+
+namespace SimpleLibrary.Tests
+{
+    [TestClass]
+    public class TestGetDuplicates
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "Input string is missing")]
+        public void Test_GetDuplicates_Shows_Error_When_String_Is_Missing()
+        {
+            Assert.Inconclusive("test pending");
+        }
+
+        [TestMethod]
+        public void Test_GetDuplicates_Returns_All_Duplicates()
+        {
+            Assert.Inconclusive("test pending");
+        }
+    }
+}
---
+++ StringDuplicates/SimpleLibraryDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleLibrary;
+
+namespace SimpleLibraryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Library Demonstration Application");
+            Console.WriteLine("Please enter a string:");
+        }
+    }
+}
