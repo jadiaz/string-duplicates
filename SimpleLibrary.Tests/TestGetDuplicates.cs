5d879fe8922289bb1f653961133fd66034df1f62
jx <jose.diaz@cdicorp.com>
2014-12-22 15:59:36 -0700

    Add performance test.
    Results of 0.9318ms for this implementation.

--- SimpleLibrary.Tests/TestGetDuplicates.cs
+++ SimpleLibrary.Tests/TestGetDuplicates.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using SimpleLibrary;
+using System.Diagnostics;
 
 namespace SimpleLibrary.Tests
 {
@@ -9,11 +10,12 @@ namespace SimpleLibrary.Tests
     public class TestGetDuplicates
     {
         StringUtilities _stringUtils;
-
+        
         [TestInitialize]
         public void Setup ()
         {
             _stringUtils = new StringUtilities();
+
         }
 
         [TestMethod]
@@ -71,6 +73,27 @@ namespace SimpleLibrary.Tests
             }
             catch (Exception)
             {
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void Test_GetDuplicates_Performance()
+        {
+            string input = "hQEMA8TOxBlJIKffAQf/SHP0RMsrkCiXwGzwfVXZXmVqCdAACz0rOtnypfu5YkT5hX1tUa1osujB6JpzPGTuByPrJwNlnPRW3VaCd+pSpHwn8naFxqsvrRIwRLgFwrBP5EG4m+lq0uVWbUmeiqq5aGUx2FAjgl59hGm2T8d3RxPRgy4BqaywBwZQbnnVPW8dP6qhOIPeLjMGwyysvn+Gtm2bFSA4PRUDnnxKcVVGQrsvmD9sElYq8AqRsAcaY9ZS7e2CQCK4nl9OBALFnO8g0fMLUVzJNfA3tdjpiNhWPwfqpvxpzI4YDwPMno+9FTjWSruIkGEG0LxXc8ca2xfW/fBTIWngwftx5d5kLRyAZdLpAQXKbPxBGFcLEOqJ0phXyN08NoD6+tAm7OojmXV2k3n+5E82pmvB0rI2h9UeV1DBWpO46mjRqdXbV2qYlpl0JKJf2x+XIzg49teKguBv4C5sJngT+GSQmYA6+DaB0yC65QHCfpKccvk0Shiu3NBappIzTcfBAkBNJgwImSu7lijw4GTH1c7463qR1IlR9Dh6ESGd/ZK3M0s1TK69tB/8F7PDxTWsseNo4QiEI04E960bAgWHJ0T8wzr5nhnbkhCzjtNiZK/eJSFBt/bBxQNnAsHCZioRApH6o7FquSUownCNJSp3/cK9b7VZ7bbMo+XDwMgQkpZWqSW2xSVZdoJHbABc4mkJM5L03pcQ9dWi6nLNTCkDdcDGYlABPRTVRBhsL3RNJA7GljIDbdoicTwp0of+B878Nukwee++i8wta4fVD50sagXmX6tdhDTh5eIyQOqE8cyFws8jtHlld9Kf0tBMBJapJzErVvSJiOh9AhHFen+y6pmYrUzyARB3gT799DDwKw2foNJPcdAmnnGBISvp83+fEL+vhCvcC8CHsYyjS1uh9HRS+X6ONfUF723cm0yxe7VqodkNsYMLFlQiMbcd0vaQYKLhKxMnVIRFP5GskxPz1nBl1bZbvZ25P6VCXG3Lyx7qtOaNn+gzXjyX5kDO9w+g1EZMH8irtWAhAwCJg2b+jEJEOcNSWm2pat8nN37Z/QXJbsnOhYVXTw3soq6ChfYAYHHtNJpStAGSD/oToDu5YouPIXPgtiCZk816V0yTnFy0k7/fLanI4mxUoWz8jqqMCg9mN2Ptt6SYzHRwIj5Vvx6DHXV75mmgEQ4OnUG7qDMTIe1ZQ33gh/msKfQjjH9zqggauCA==IW7l";
+            try
+            {
+                Stopwatch sw = Stopwatch.StartNew();
+                string dups = _stringUtils.GetDuplicates(input);
+                sw.Stop();
+
+                Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+                Trace.WriteLine(sw.Elapsed.TotalMilliseconds);
+                Trace.Flush();
+            }
+            catch (Exception)
+            {
+                
                 throw;
             }
         }
